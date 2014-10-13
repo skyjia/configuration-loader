786c3a47c73d2b2a9c79ff997b8d22f90b91f239
Sky JIA <me@skyjia.com>
2014-10-13 12:17:45 +0800

    init

---
+++ configuration-loader/configuration_loader.js
@@ -0,0 +1,42 @@
+module.exports = createLoader;
+
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
+var yaml = require('js-yaml');
+
+function createLoader(defaultConfigFilePath, currentConfigFilePath){
+    return new ConfigurationLoader(defaultConfigFilePath, currentConfigFilePath);
+}
+
+function ConfigurationLoader(defaultConfigFilePath, currentConfigFilePath) {
+    this.defaultConfigFilePath = path.resolve(defaultConfigFilePath);
+    this.currentConfigFilePath = path.resolve(currentConfigFilePath);
+
+    this.config = {};
+    return this;
+};
+
+ConfigurationLoader.prototype.reloadFromFile = function (configFilePath) {
+    this.currentConfigFilePath = configFilePath;
+    this.reload();
+};
+
+ConfigurationLoader.prototype.reload = function () {
+
+    // Check config files existing
+    if (!fs.existsSync(this.defaultConfigFilePath)) {
+        throw new Error("Default configuration file is missing.");
+    }
+
+    if (!fs.existsSync(this.currentConfigFilePath)) {
+        throw new Error("Current configuration file is missing.");
+    }
+
+    // Load config
+    var defaultConfig = yaml.safeLoad(fs.readFileSync(this.defaultConfigFilePath, 'utf8'), {filename: this.defaultConfigFilePath});
+    var currentConfig = yaml.safeLoad(fs.readFileSync(this.currentConfigFilePath, 'utf8'), {filename: this.currentConfigFilePath});
+    this.config = _.merge(defaultConfig, currentConfig);
+
+    return _.cloneDeep(this.config);
+};
