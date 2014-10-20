338c99cfa8b38bb98246e4d4f6265e57d8dc2c44
Sky JIA <me@skyjia.com>
2014-10-20 12:54:51 +0800

    updated

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+    // Load the plugin that provides the "jshint" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Load the plugin that provides the "mochaTest" task.
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    // Project configuration.
+    grunt.initConfig({
+        // Configure a jshint task
+        jshint: {
+            options: {
+                jshintrc: true,
+                reporter: require('jshint-stylish')
+            },
+
+            target1: ['Gruntfile.js', 'index.js', 'lib/**/*.js']
+        },
+
+        // Configure a mochaTest task
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'test/test_results.txt', // Optionally capture the reporter output to a file
+                    quiet: false // Optionally suppress output to standard out (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    });
+
+    // Define the default task
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+    // Define the test task
+    grunt.registerTask('test', ['mochaTest']);
+};
--- test/index.js
+++ test/index.js
@@ -3,9 +3,25 @@ var should = require('chai').should(),
 
 var createLoader = CS.createLoader;
 
-
 describe('#createLoader', function () {
-    it('load default.yml and custom.yml', function () {
+    it('load default.yml only', function () {
+        var loader = createLoader('./test/conf/default.yml');
+        var cfg = loader.reload();
+
+        var v = cfg.field3.field3_1;
+        v.should.equal("value3_1");
+    });
+
+    it('load default.yml and custom.yml by array', function () {
+        var loader = createLoader(['./test/conf/default.yml', './test/conf/custom.yml']);
+        var cfg = loader.reload();
+
+        var f2 = cfg.field2;
+        f2.should.equal("value2-");
+
+    });
+
+    it('load default.yml and custom.yml by dynamic arguments', function () {
         var loader = createLoader('./test/conf/default.yml', './test/conf/custom.yml');
         var cfg = loader.reload();
 
